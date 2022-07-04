faf0907537ebeb4535f60a2980b771e9801fc173
yoshi23s <classa1990@gmail.com>
2022-07-04 18:04:28 +0900

    プロトタイプトップページ表示まで

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,11 @@
 class PrototypesController < ApplicationController
   
   def index
+    @prototypes = Prototype.includes(:user).order("created_at DESC")
   end
 
   def new
-    @prototype =Prototype.new
+    @prototype = Prototype.new
   end
 
   def create
