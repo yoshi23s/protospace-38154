4bb67e9c431e7f82010d5ec20eeeef80fa907d66
yoshi23s <classa1990@gmail.com>
2022-07-07 03:00:52 +0900

    Revert "initial commit"
    This reverts commit c3b817554f00e4309a293086b96bae4476e01a04.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -44,6 +44,7 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
+
   private
   def prototype_params
     params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
