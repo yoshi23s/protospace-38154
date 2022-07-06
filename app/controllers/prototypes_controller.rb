62821d3028789e07adacf0512840bb153f07cda1
yoshi23s <classa1990@gmail.com>
2022-07-06 11:35:16 +0900

    削除機能実装完了まで

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -35,6 +35,12 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
 
   private
   def prototype_params
