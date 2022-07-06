ff853e19a2fe6de6dcfe9e81b6984c3010847828
yoshi23s <classa1990@gmail.com>
2022-07-06 11:03:04 +0900

    詳細機能実装まで

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -17,6 +17,11 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+
   private
   def prototype_params
     params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
