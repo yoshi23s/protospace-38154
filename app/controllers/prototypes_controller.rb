0984c3188e1344f2624d21ce66165d88ffba2435
yoshi23s <classa1990@gmail.com>
2022-07-06 11:26:51 +0900

    編集機能実装まで

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,6 +21,20 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if
+      @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
 
   private
   def prototype_params
